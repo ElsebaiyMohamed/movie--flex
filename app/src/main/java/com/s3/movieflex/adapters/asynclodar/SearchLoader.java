58592f8011d25a6b53bc9b23f5b2091386523100
Men3m <abdelmonemanwr7777@gmail.com>
2021-09-01 02:07:35 +0200

    SearchLoader klakit 10 mra

--- app/src/main/java/com/s3/movieflex/adapters/asynclodar/SearchLoader.java
+++ app/src/main/java/com/s3/movieflex/adapters/asynclodar/SearchLoader.java
@@ -6,30 +6,116 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.loader.content.AsyncTaskLoader;
 
+import com.s3.movieflex.model.Cast;
 import com.s3.movieflex.model.Movie;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class SearchLoader extends AsyncTaskLoader<ArrayList<Movie>> {
 
-    private String filmName;
-    public SearchLoader(Context context, String filmName) {
+    final String UTubeLink = "https://www.youtube.com/watch?v=";
+    private static final String frontLink="https://api.themoviedb.org/3/search/movie?api_key=452671fab9e5a7eb0349b6139855d282&query=";
+    ArrayList<Movie> movies = new ArrayList<>();
+    String url = null;
+    final String trial = "/videos";
+    final String actors = "/credits";
+    public SearchLoader(@NonNull Context context, String url) {
         super(context);
-        this.filmName=filmName;
-    }
-
-    public SearchLoader(@NonNull Context context) {
-        super(context);
-    }
-
-    @Override
-    protected void onStartLoading() {
-        forceLoad();
+        this.url = url;
     }
 
     @Nullable
     @Override
     public ArrayList<Movie> loadInBackground() {
-        return null;
+        // Parsing JSON Object
+        try {
+            String jsonString = getHttpRequest(new URL(frontLink + url));
+            JSONObject jsonObject = new JSONObject(jsonString);
+            JSONArray jsonArray = jsonObject.getJSONArray("results");
+            final int resultLength = jsonArray.length();
+            for (int i = 0; i < resultLength; i++) {
+                JSONObject movies = jsonArray.getJSONObject(i);
+                int id = movies.getInt("id");
+                double rate = movies.getDouble("vote_average");
+                String title = null;
+                title = movies.getString("title");
+                String coverLink = movies.getString("backdrop_path");
+                String poster = movies.getString("poster_path");
+                String description = movies.getString("overview");
+
+                String jsonTrial = getHttpRequest(new URL(frontLink +id + trial));
+                JSONObject trialObject = new JSONObject(jsonTrial);
+                String trial1 = null;
+                if (trialObject.has("key"))
+                    trial1 = UTubeLink + trialObject.getString("key");
+
+                String jsonCast = getHttpRequest(new URL(frontLink + id + actors));
+                JSONObject castObject = new JSONObject(jsonCast);
+                JSONArray castArray = castObject.getJSONArray("cast");
+                ArrayList<Cast> actorsList = new ArrayList<>();
+                final int castLength = castArray.length();
+                for (int j = 0; j < castLength; j++) {
+                    JSONObject cast2 = castArray.getJSONObject(j);
+                    if (cast2.has("name"))
+                        actorsList.add(new Cast(cast2.getString("name"), cast2.getString("profile_path")));
+                }
+                this.movies.add(new Movie(-1, id, title, description, poster, coverLink, rate, trial1, actorsList));
+            }
+        } catch (JSONException | IOException e) {
+            e.printStackTrace();
+        }
+        return movies;
+    }
+
+    private String getHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+        InputStream inputStream = null;
+        HttpsURLConnection urlConnection = null;
+        try {
+            urlConnection = (HttpsURLConnection) url.openConnection();  // open connection + casting to HTTPS
+            urlConnection.setRequestMethod("GET");                      // Type of connection is getting data
+            urlConnection.setReadTimeout(5000);                        // Get Data Time = 10 seconds
+            urlConnection.setConnectTimeout(10000);                     // Bad Connection Time = 20 seconds
+            urlConnection.connect();
+            inputStream = urlConnection.getInputStream();
+            jsonResponse = readInputStream(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    // maybe there is an error here in return type of function
+    private String readInputStream(InputStream inputStream) throws IOException {
+        StringBuilder result = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+            BufferedReader inputReader = new BufferedReader(inputStreamReader);
+            String line = inputReader.readLine();
+            while (line != null) {
+                result.append(line);
+                line = inputReader.readLine();
+            }
+        }
+        return result.toString();
     }
 }
--- app/src/main/java/com/s3/movieflex/ui/fragments/SearchFragment.java
+++ app/src/main/java/com/s3/movieflex/ui/fragments/SearchFragment.java
@@ -6,27 +6,40 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.s3.movieflex.R;
 import com.s3.movieflex.adapters.MovieFavAdapter;
 import com.s3.movieflex.adapters.MovieItemClickListener;
+import com.s3.movieflex.adapters.asynclodar.SearchLoader;
 import com.s3.movieflex.model.Cast;
 import com.s3.movieflex.model.Movie;
 import com.s3.movieflex.ui.MovieDetailActivity;
 
 import java.util.ArrayList;
 
-public class SearchFragment extends Fragment implements MovieItemClickListener {
+public class SearchFragment extends Fragment implements MovieItemClickListener, LoaderManager.LoaderCallbacks {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+
+    Button searchButton;
+    EditText searchEditText;
+
+    private String str;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -57,8 +70,9 @@ public class SearchFragment extends Fragment implements MovieItemClickListener {
 
     MovieFavAdapter adapter;
     ArrayList<Movie> film = new ArrayList<>();
-    ArrayList<Cast> casts=new ArrayList<>();
+    ArrayList<Cast> casts = new ArrayList<>();
     RecyclerView search_result;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -66,22 +80,44 @@ public class SearchFragment extends Fragment implements MovieItemClickListener {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_search, container, false);
 
-        adapter = new MovieFavAdapter(getContext(), film, (MovieItemClickListener) this);
         search_result = view.findViewById(R.id.search_result);
-        search_result.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false));
-        search_result.setAdapter(adapter);
+        searchButton = view.findViewById(R.id.btnSearch);
+        searchEditText = view.findViewById(R.id.searchEditText);
+
+        searchButton.setOnClickListener(view1 -> {
+            str = searchEditText.getText().toString();
+            LoaderManager.getInstance(this).initLoader(1, null, this);
+        });
         return view;
     }
 
     @Override
     public void onMovieClick(Movie movie, ImageView movieImageView) {
-        if(getActivity()!=null) {
+        if (getActivity() != null) {
 
             Intent intent = new Intent(getActivity(), MovieDetailActivity.class);
-            intent.putExtra("movie",movie);
+            intent.putExtra("movie", movie);
 
             ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(getActivity(), movieImageView, "sharedName");
             startActivity(intent, options.toBundle());
         }
+    }
+
+    @NonNull
+    @Override
+    public Loader onCreateLoader(int id, @Nullable Bundle args) {
+        return new SearchLoader(getContext(), str);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader loader, Object data) {
+        adapter = new MovieFavAdapter(getContext(), (ArrayList<Movie>) data, (MovieItemClickListener) this);
+        search_result.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false));
+        search_result.setAdapter(adapter);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader loader) {
+
     }
 }
